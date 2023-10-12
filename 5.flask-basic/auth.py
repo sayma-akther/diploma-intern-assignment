d12a937a0e827bfd7bb613466e22472ac216f667
sayma-akther <saymaakther126@gmail.com>
2023-10-12 17:12:20 +0600

    added Title

--- 5.flask-basic/auth.py
+++ 5.flask-basic/auth.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 
 # Created a Blueprint
-basic_controller = Blueprint(
-    "basic_controller",
+auth_controller = Blueprint(
+    "auth_controller",
     __name__,
     url_prefix="/",
     template_folder="template-assets/templates",
@@ -16,39 +16,39 @@ basic_controller = Blueprint(
 
 
 
-@basic_controller.route('/', methods=['GET'])
-@basic_controller.route('/registration', methods=['GET'])
+@auth_controller.route('/', methods=['GET'])
+@auth_controller.route('/registration', methods=['GET'])
 def registration():
     return render_template("auth/registration.html")
 
 
-@basic_controller.route('/login', methods=['GET'])
+@auth_controller.route('/login', methods=['GET'])
 def login():
     return render_template("auth/login.html")
 
 
-@basic_controller.route('/forgot-password', methods=['GET'])
+@auth_controller.route('/forgot-password', methods=['GET'])
 def forgotPassword():
     return render_template("auth/forgot-password.html")
 
 
-@basic_controller.route('/set-password', methods=['GET'])
+@auth_controller.route('/set-password', methods=['GET'])
 def setPassword():
     return render_template("auth/set-password.html")
 
 
-@basic_controller.route('/otp', methods=['GET'])
+@auth_controller.route('/otp', methods=['GET'])
 def otp():
     return render_template("auth/otp.html")
 
 
-@basic_controller.route("/reset-success", methods=['GET'])
+@auth_controller.route("/reset-success", methods=['GET'])
 def resetSuccess():
     return render_template('auth/reset-success.html')
 
 
 # Register the Blueprint
-app.register_blueprint(basic_controller)
+app.register_blueprint(auth_controller)
 
 
 if __name__ == '__main__':
